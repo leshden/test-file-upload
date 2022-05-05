23ffd6d906f261a763990fccbd2d45bd30548122
Viktor Denikin <leshden@yandex.ru>
2022-05-05 18:47:40 +0300

    progress

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,14 @@
 import React, {Fragment, useState} from 'react';
 import axios from 'axios';
+import Message from './Message';
+import Progress from './Progress';
 
 const FileUpload = () => {
   const [file, setFile] = useState('');
   const [filename, setFilename] = useState('');
   const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState('');
+  const [uploadPercentage, setUploadPercentage] = useState(0);
 
   const onChange = e => {
     setFile(e.target.files[0]);
@@ -21,17 +25,24 @@ const FileUpload = () => {
       const res = await axios.post('/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
+        },
+        onUploadProgress: progressEvent => {
+          setUploadPercentage(parseInt(Math.round((progressEvent.loaded * 100)/progressEvent.total)));
+          setTimeout(()=>setUploadPercentage(0), 10000);
         }
-      })
+
+      });
+
+
 
       const {fileName, filePath} = res.data;
       setUploadedFile({fileName, filePath});
-
+      setMessage('File Uploaded');
     } catch(err) {
       if (err.response.status == 500) {
-        console.log('Problem with Server');
+        setMessage('Problem with Server');
       } else {
-        console.log(err.response.data.msg);
+        setMessage(err.response.data.msg);
       }
     }
 
@@ -39,10 +50,12 @@ const FileUpload = () => {
 
   return(
     <Fragment>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmitForm}>
         <div className='custom-file mb-4'>
           <input className="form-control" type="file" id="formFileMultiple" multiple onChange={onChange} />
         </div>
+        <Progress percentage = {uploadPercentage} />
         <input type='submit' value='Upload' className='btn btn-primary mt-4 form-control' />
       </form>
       {
